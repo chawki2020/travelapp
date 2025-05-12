9b9905045ab9b91df9e2cce869d1df100c6411d8
chawki2020 <41899166+chawki2020@users.noreply.github.com>
2025-05-12 12:18:15 +0000

    Update files

---
+++ my-webview-app/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <div style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
+      <iframe
+        src="https://mixmap-elementor.travelerwp.com"
+        style={{ width: '100%', height: '100%', border: 'none' }}
+        title="Webview Content"
+      ></iframe>
+    </div>
+  );
+}
+
+export default App
